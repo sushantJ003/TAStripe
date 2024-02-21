431baf987cdadc5d5e71dc2508a4c5c98436393e
Sushant <sushant.jugran@teamx.one>
2024-02-21 11:22:07 +0530

    added payment manager

--- Sources/TAStripe/TAPaymentManager.swift
+++ Sources/TAStripe/TAPaymentManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Stripe
+
+public enum PaymentMode {
+    case stripe
+    case paypal
+}
+
+public struct TAPaymentManager {
+    var mode: PaymentMode = .stripe
+    
+    public init(publishableKey: String, appleMerchantIdentifier: String, companyName: String, paymentMode: PaymentMode) {
+        
+        mode = paymentMode
+        
+        switch paymentMode {
+        case .stripe:
+            STPAPIClient.shared.publishableKey = publishableKey
+            
+            STPPaymentConfiguration.shared.appleMerchantIdentifier = appleMerchantIdentifier
+            
+            STPPaymentConfiguration.shared.companyName = companyName
+        case .paypal:
+            break
+        }
+    }
+}
