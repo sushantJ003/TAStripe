91e66bfeb607e327dca3978d9161d43232d265bf
Sushant <sushantjugran003@gmail.com>
2024-04-10 07:54:26 +0530

    minor fix

--- Sources/TAStripe/Stripe/StripeManager.swift
+++ Sources/TAStripe/Stripe/StripeManager.swift
@@ -50,8 +50,8 @@ class StripeManager: StripeManagerProtocol {
                                 
                 sheet.present(from: controller) { [weak self] paymentResult in
                     result = self?.getPaymentResult(stripeResult: paymentResult) ?? .cancelled
-                    self?.completion(result)
                     controller.dismiss(animated: true)
+                    self?.completion(result)                    
                 }
             }
     }
